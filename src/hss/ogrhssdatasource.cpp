b62f76e74048b3a64352f7eadcf98d7d2b048d57
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-09-17 19:28:34 +0300

    progreso

--- src/hss/ogrhssdatasource.cpp
+++ src/hss/ogrhssdatasource.cpp
@@ -36,7 +36,7 @@ OGRErr OGRHSSDataSource::exportFeature(OGRFeature *poFeature)
     }
 
     char *pszValue = nullptr;
-    if( poSRS->exportToProj4( &pszValue ) == OGRERR_NONE )
+    if( poGeom->exportToWkt &pszValue ) == OGRERR_NONE )
     {
         VSIFPrintfL( fp, "HSS: FEATURE Geom \"%s\"\n", pszValue );
     }
